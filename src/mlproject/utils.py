e2c4d7d35d6e27cca3a4e474c3c85b46d9bc2e63
Dakshgupta25 <dakshgupta25306@gmail.com>
2026-02-23 23:04:09 +0530

    ingestion with sql

--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -4,5 +4,29 @@ from src.mlproject.exception import CustomException
 from src.mlproject.logger import logging
 import pandas as pd
 from dotenv import load_dotenv
+import pymysql
+
+load_dotenv()
+
+host=os.getenv('host')
+user=os.getenv('user')
+passw=os.getenv('password')
+db=os.getenv('db')
 
 def read_sql_data():
+    logging.info("Reading SQL database started")
+    try:
+        mydb=pymysql.connect(
+            host=host,
+            user=user,
+            password=passw,
+            db=db
+        )
+        logging.info("connection stabilished",mydb)
+        df=pd.read_sql_query('Select * from students',mydb)
+        print(df.head())
+        
+        return df
+
+    except Exception as e:
+        raise CustomException(e,sys)
